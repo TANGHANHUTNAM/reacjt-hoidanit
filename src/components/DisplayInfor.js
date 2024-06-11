43ff5822a28259e4e5c06036ade4a9c0fc1a1a11
namtang83 <143093635+namtang83@users.noreply.github.com>
2024-06-11 09:33:46 +0700

    bai 33

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,15 +2,38 @@ import React from "react";
 import "./DisplayInfor.scss";
 // import logo from "./../logo.svg";
 class DisplayInfor extends React.Component {
-  state = {
-    isShowListUser: true,
-  };
+  constructor(props) {
+    console.log("call constructor 1");
+    super(props);
+    this.state = {
+      isShowListUser: true,
+    };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    setTimeout(() => {
+      document.title = "Nhut Nam";
+    }, 3000);
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("componentDisUpdate", this.props, prevProps);
+    if (this.props.listUser !== prevProps.listUser) {
+      if (this.props.listUser.length === 5) {
+        alert("You have 5 users");
+      }
+    }
+  }
+
   handleShowHide = () => {
     this.setState({
       isShowListUser: !this.state.isShowListUser,
     });
   };
+
   render() {
+    console.log("render");
     const { listUser } = this.props;
     return (
       <div className="display-infor-container">
