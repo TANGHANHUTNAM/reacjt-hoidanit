d7ec7fe79f217e9ad29493b1b168f04397ac6a40
namtang83 <143093635+namtang83@users.noreply.github.com>
2024-06-13 13:23:36 +0700

    52

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,8 +1,10 @@
+import videoHomepage from "../../assets/video-homepage.mp4";
 const HomePage = (props) => {
   return (
     <div className="homepage-container">
-      <h1>Home Page</h1>
-      <p>Welcome to the Home Page</p>
+      <video loop autoPlay muted>
+        <source src={videoHomepage} type="video/mp4" />
+      </video>
     </div>
   );
 };
