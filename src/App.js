1b582f79485267af7adcf606f82be76b34a01e32
namtang83 <143093635+namtang83@users.noreply.github.com>
2024-06-13 11:14:40 +0700

    bai 48

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import { Link } from "react-router-dom";
 const App = () => {
   return (
     <div className="app-container">
       <Header />
+      <div>
+        testlink
+        <div>
+          <button>
+            <Link to="/users">Go to User Page</Link>
+          </button>
+          <button>
+            <Link to="/admins">Go to User Page</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
