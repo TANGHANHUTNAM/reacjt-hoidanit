e871ad4b3ce2d98ee05aacfebe92564bb7832104
namtang83 <143093635+namtang83@users.noreply.github.com>
2024-06-19 11:25:01 +0700

    bai 78

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -3,11 +3,13 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { postLogin } from "../../services/apiService";
 import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { doLogin } from "../../redux/action/userAction";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
   const validateEmail = (email) => {
     return String(email)
       .toLowerCase()
@@ -31,6 +33,7 @@ const Login = () => {
     let data = await postLogin(email, password);
 
     if (data && +data.EC === 0) {
+      dispatch(doLogin(data));
       toast.success(data.EM);
       navigate("/");
     }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-// import NavDropdown from "react-bootstrap/NavDropdown";
+import { useSelector } from "react-redux";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import { NavLink, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const isAuthencated = useSelector((state) => state.user.isAuthencated);
+  const account = useSelector((state) => state.user.account);
   const navigate = useNavigate();
   const handleLogin = () => {
     navigate("/login");
@@ -32,27 +35,31 @@ const Header = () => {
             </NavLink>
           </Nav>
           <Nav>
-            <button
-              className="btn-login"
-              onClick={() => {
-                handleLogin();
-              }}
-            >
-              Login
-            </button>
-            <button
-              className="btn-signup"
-              onClick={() => {
-                handleSignUp();
-              }}
-            >
-              Sign up
-            </button>
-            {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
-              <NavDropdown.Item>Login</NavDropdown.Item>
-              <NavDropdown.Item>Log out</NavDropdown.Item>
-              <NavDropdown.Item>Profile</NavDropdown.Item>
-            </NavDropdown> */}
+            {isAuthencated === false ? (
+              <>
+                <button
+                  className="btn-login"
+                  onClick={() => {
+                    handleLogin();
+                  }}
+                >
+                  Login
+                </button>
+                <button
+                  className="btn-signup"
+                  onClick={() => {
+                    handleSignUp();
+                  }}
+                >
+                  Sign up
+                </button>
+              </>
+            ) : (
+              <NavDropdown title="Setting" id="basic-nav-dropdown">
+                <NavDropdown.Item>Log out</NavDropdown.Item>
+                <NavDropdown.Item>Profile</NavDropdown.Item>
+              </NavDropdown>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
