34f5e64e678f42a7a37b41b153b98895bea9ca23
namtang83 <143093635+namtang83@users.noreply.github.com>
2024-06-23 22:52:59 +0700

    bai 103

--- src/components/User/Question.js
+++ src/components/User/Question.js
@@ -1,15 +1,33 @@
 import _ from "lodash";
+import { useState } from "react";
+import Lightbox from "react-awesome-lightbox";
 const Question = (props) => {
+  const [isPreviewImage, setIsPreviewImage] = useState(false);
   const { data, index } = props;
+
   if (_.isEmpty(data)) return <></>;
+
   const handleCheckbox = (event, aId, qId) => {
     props.handleCheckbox(aId, qId);
   };
+
   return (
     <>
       {data.image ? (
         <div className="q-image">
-          <img src={`data:image/jpeg;base64,${data.image}`} alt="" />
+          <img
+            style={{ cursor: "pointer" }}
+            onClick={() => setIsPreviewImage(true)}
+            src={`data:image/jpeg;base64,${data.image}`}
+            alt=""
+          />
+          {isPreviewImage === true && (
+            <Lightbox
+              image={`data:image/jpeg;base64,${data.image}`}
+              title={"Question Image"}
+              onClose={() => setIsPreviewImage(false)}
+            ></Lightbox>
+          )}
         </div>
       ) : (
         <div className="q-image"></div>
